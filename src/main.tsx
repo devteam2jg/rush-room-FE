562272dcfe522986a0e964afa27164b727792b88
giujae <alacran6924@gmail.com>
2024-10-21 01:45:11 +0900

    feat(reactquery): add react query setting

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,16 @@ import { ChakraProvider } from '@chakra-ui/react';
 import router from './routes';
 import './index.css';
 import theme from './utils/theme';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ChakraProvider theme={theme}>
-      <RouterProvider router={router} />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <RouterProvider router={router} />
+      </ChakraProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
