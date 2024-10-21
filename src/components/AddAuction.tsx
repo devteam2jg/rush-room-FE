3737a5c25231cb666f477622946d1d9bfbcc5560
soyeon <thdusmm6@gmail.com>
2024-10-21 17:05:08 +0900

    chore(auction deatail page): 배경 색상 변경

--- src/components/AddAuction.tsx
+++ src/components/AddAuction.tsx
@@ -2,13 +2,13 @@ import { Box, Button, Heading, VStack } from '@chakra-ui/react';
 
 export default function AddAuction() {
   return (
-    <Box p={4} textAlign="center">
+    <Box p={4} textAlign="center" bg="white">
       <VStack spacing={4} align="flex-start">
         <Heading as="h1" size="md" textAlign="left">
           등록된 물품이 없어요. <br />
           버튼을 눌러 등록해 보세요.
         </Heading>
-        <Heading as="h2" size="sm" color="GrayText" textAlign="left">
+        <Heading as="h3" size="sm" color="GrayText" textAlign="left">
           누구든지 경매에 참여할 수 있어요.
         </Heading>
 
--- src/components/AuctionList.tsx
+++ src/components/AuctionList.tsx
@@ -2,7 +2,7 @@ import { Box, Heading, VStack } from '@chakra-ui/react';
 
 export default function AuctionList() {
   return (
-    <Box p={4} textAlign="center">
+    <Box p={4} textAlign="center" bg="white">
       <VStack spacing={4} align="flex-start">
         <Heading as="h5" size="md" textAlign="left">
           경매 물품 리스트
