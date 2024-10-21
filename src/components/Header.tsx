c1195af208271568186b71db956f24d55aca20bb
soyeon <thdusmm6@gmail.com>
2024-10-21 20:29:18 +0900

    chore(mainpage) : logo 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,12 @@
-import { Box, Flex, Text, useColorModeValue } from '@chakra-ui/react';
+import { Box, Flex, Image, useColorModeValue } from '@chakra-ui/react';
 
 export default function Header() {
   return (
     <Box bg={useColorModeValue('white', 'gray.900')} px={4} py={2} shadow="sm">
-      <Flex h={16} alignItems="center" justifyContent="center">
-        <Text fontSize="2xl" fontWeight="bold" color="purple.600">
-          Rush Room
-        </Text>
-      </Flex>
+      <Flex h={3} alignItems="center" justifyContent="center" />
+      <Box>
+        <Image src="images/logo.png" alt="logo" width="103px" height="52px" />
+      </Box>
     </Box>
   );
 }
