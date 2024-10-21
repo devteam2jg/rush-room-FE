c41136b565fbcf1d146dd20c5f8ef737aed539cc
soyeon <thdusmm6@gmail.com>
2024-10-21 15:35:34 +0900

    chore(main.tsx) : import  ./index.css 에서 오류가 나서 잠시 주석처리 해놓았습니다.

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import router from './routes';
-import './index.css';
+// import './index.css';
 import theme from './utils/theme';
 
 createRoot(document.getElementById('root')!).render(
