77e6e4e471aa7c30bde9275fb77039d987001cf2
soyeon <thdusmm6@gmail.com>
2024-10-21 13:12:41 +0900

    feat: navigator icon의 image 소스 링크 수정

--- src/components/Navigator.tsx
+++ src/components/Navigator.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Image, Text, Link } from '@chakra-ui/react'
+import { Box, Flex, Image, Text, Link } from '@chakra-ui/react';
 
 export default function Navigator() {
   return (
@@ -13,61 +13,53 @@ export default function Navigator() {
       borderColor="gray.200"
     >
       <Flex justify="space-around" align="center" p={2}>
-        <Link href="#">
+        <Link href="/home">
           <Flex direction="column" align="center">
-            <Image 
-            src='../../public/images/home_icon.png'
-            alt="홈"
-            boxSize="24px"
-            />
+            <Image src="images/home_icon.png" alt="홈" boxSize="24px" />
             <Text fontSize="sm">홈</Text>
           </Flex>
-        </Link> 
+        </Link>
 
-        <Link href="#">
+        <Link href="/">
           <Flex direction="column" align="center">
-          <Image 
-            src='../../public/images/list_icon.png'
-            alt="경매 리스트"
-            boxSize="24px"
+            <Image
+              src="images/list_icon.png"
+              alt="경매 리스트"
+              boxSize="24px"
             />
             <Text fontSize="sm">경매 리스트</Text>
           </Flex>
         </Link>
 
-        <Link href="#">
+        <Link href="/">
           <Flex direction="column" align="center">
-          <Image 
-            src='../../public/images/add_icon.png'
-            alt="새로운 경매"
-            boxSize="24px"
-            />
+            <Image src="images/add_icon.png" alt="새로운 경매" boxSize="24px" />
             <Text fontSize="sm">새로운 경매</Text>
           </Flex>
         </Link>
 
-        <Link href="#">
+        <Link href="/">
           <Flex direction="column" align="center">
-          <Image 
-            src='../../public/images/participant_icon.png'
-            alt="참여중 경매"
-            boxSize="24px"
+            <Image
+              src="images/participant_icon.png"
+              alt="참여중 경매"
+              boxSize="24px"
             />
             <Text fontSize="sm">참여중 경매</Text>
           </Flex>
         </Link>
 
-        <Link href="#">
+        <Link href="/">
           <Flex direction="column" align="center">
-          <Image 
-            src='../../public/images/mypage_icon.png'
-            alt="마이페이지"
-            boxSize="24px"
+            <Image
+              src="images/mypage_icon.png"
+              alt="마이페이지"
+              boxSize="24px"
             />
             <Text fontSize="sm">마이페이지</Text>
           </Flex>
         </Link>
       </Flex>
     </Box>
-  )
+  );
 }
