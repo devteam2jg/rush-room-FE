a99492f60f3b966c9e3fdf902392f6a5e4967a5b
giujae <alacran6924@gmail.com>
2024-10-22 13:58:46 +0900

    refactor: minor fixes

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import router from './routes';
 import './index.css';
 import theme from './utils/theme';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
 
