ea64c7e960d4dd7d5da87d092956ca1264568eea
giujae <alacran6924@gmail.com>
2024-10-22 13:57:43 +0900

    feat(api): change api url to real url

--- src/pages/LoginPage/Login.tsx
+++ src/pages/LoginPage/Login.tsx
@@ -2,54 +2,51 @@ import { Text, Box, Button, Image, Flex } from '@chakra-ui/react';
 import kakaoLogo from '../../assets/images/kakaoLogo.png';
 import ServiceLogo from '../../assets/images/serviceLogo.png';
 
-const Login = () => {
+const baseAPIURL = import.meta.env.VITE_APP_API_BASE_URL;
+
+function Login() {
   const handleKakaoLoginButtonClick = () => {
-    // 추후에 카카오 로그인 api url로 변경
-    window.location.href = 'https://github.com/devteam2jg/rush-room-FE';
+    window.location.href = `${baseAPIURL}/api/v1/auth/kakao`;
   };
   return (
-    <>
-      <Flex
-        width={'100%'}
-        height={'100vh'}
-        flexDirection={'column'}
-        justifyContent={'flex-end'}
-        alignItems={'center'}
+    <Flex
+      width="100%"
+      height="100vh"
+      flexDirection="column"
+      justifyContent="flex-end"
+      alignItems="center"
+    >
+      <Image width="157px" src={ServiceLogo} marginBottom="10px" />
+      <Text
+        color="blackAlpha.900"
+        fontWeight={500}
+        textAlign="center"
+        marginBottom="30px"
       >
-        <Image width={'157px'} src={ServiceLogo} marginBottom={'10px'} />
-        <Text
-          color={'blackAlpha.900'}
-          fontWeight={500}
-          textAlign={'center'}
-          marginBottom={'30px'}
+        간편하게 로그인 하고
+        <br />
+        다양한 서비스를 이용해보세요.
+      </Text>
+      <Box>
+        <Button
+          backgroundColor="#fee500"
+          borderRadius="12px"
+          size="md"
+          width="290px"
+          height="50px"
+          color="#000000d9"
+          _hover={{ backgroundColor: '#e6ce00' }}
+          leftIcon={<Image src={kakaoLogo} alt="Kakao Logo" boxSize="25px" />}
+          marginBottom="230px"
+          onClick={handleKakaoLoginButtonClick}
         >
-          간편하게 로그인 하고
-          <br />
-          다양한 서비스를 이용해보세요.
-        </Text>
-        <Box>
-          <Button
-            backgroundColor="#fee500"
-            borderRadius={'12px'}
-            size={'md'}
-            width={'290px'}
-            height={'50px'}
-            color={'#000000d9'}
-            _hover={{ backgroundColor: '#e6ce00' }}
-            leftIcon={
-              <Image src={kakaoLogo} alt="Kakao Logo" boxSize={'25px'} />
-            }
-            marginBottom={'230px'}
-            onClick={handleKakaoLoginButtonClick}
-          >
-            <Text fontSize={'16px'} fontWeight={700}>
-              카카오 로그인
-            </Text>
-          </Button>
-        </Box>
-      </Flex>
-    </>
+          <Text fontSize="16px" fontWeight={700}>
+            카카오 로그인
+          </Text>
+        </Button>
+      </Box>
+    </Flex>
   );
-};
+}
 
 export default Login;
