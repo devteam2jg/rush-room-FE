f4650ef07392187c605886f57207d5062caac6c6
giujae <alacran6924@gmail.com>
2024-10-23 22:05:19 +0900

    chore: fix for build

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  // Load environment variables based on the mode (e.g., 'development', 'production')
-  const env = loadEnv(mode, process.cwd(), '');
-
-  return {
-    base: '/', // 이것은 배포 시의 public path
-    plugins: [react()],
-    server: {
-      host: true, // 개발 서버 호스트
-      port: env.VITE_PORT || 3000,
-    },
-  };
+export default defineConfig({
+  plugins: [react()],
 });
