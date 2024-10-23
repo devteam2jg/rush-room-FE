4cb433852525a75ffd9353f22be8a2d12209dde0
giujae <alacran6924@gmail.com>
2024-10-23 10:17:07 +0900

    refactor(AuctionItem): add updateItem for storing auctionItemId

--- src/hooks/useCreateAuctionItem.tsx
+++ src/hooks/useCreateAuctionItem.tsx
@@ -1,23 +1,38 @@
 import { useMutation } from '@tanstack/react-query';
-import axios from 'axios';
+import axiosInstance from '../utils/AxiosInstance';
 import useShowToast from './useShowToast';
 import useAuctionItemStore from '../store/AuntionItemStore';
 
 const useCreateAuctionItem = () => {
-  const { getFormItemData, resetItemForm } = useAuctionItemStore();
+  const auctionId = '844a6e6d-b685-4c60-ad82-6a96891698fe';
+  const { getFormItemData, resetItemForm, updateItemField } =
+    useAuctionItemStore();
   const showToast = useShowToast();
 
   const createAuctionItem = async () => {
-    const data = getFormItemData();
-    const response = await axios.post('/api/post', data);
+    const formData = getFormItemData();
+
+    const data = {
+      title: formData.itemName,
+      description: formData.description,
+      startPrice: Number(formData.price),
+    };
+
+    console.log(typeof formData.price);
+
+    const response = await axiosInstance.post(
+      `/auction/${auctionId}/auction-item`,
+      data
+    );
     return response.data;
   };
 
   return useMutation({
     mutationFn: createAuctionItem,
     onSuccess: (data) => {
-      resetItemForm();
       console.log(data);
+      resetItemForm();
+      updateItemField('auctionItemId', data);
       showToast('Success', '아이템 생성이 완료되었습니다.', 'success');
     },
     onError: (error) => {
