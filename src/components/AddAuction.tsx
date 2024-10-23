5b23c3d5c40e0d7e90fd20b462b259629496ed60
giujae <alacran6924@gmail.com>
2024-10-23 20:23:09 +0900

    refactor(AuctionCreate): create item to a specific aution page

--- src/components/AddAuction.tsx
+++ src/components/AddAuction.tsx
@@ -1,16 +1,17 @@
 import { Box, Button, Heading, VStack } from '@chakra-ui/react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 export default function AddAuction() {
   const navigate = useNavigate();
+  const { id } = useParams();
   return (
     <Box p={4} textAlign="center" bg="white">
       <VStack spacing={4} align="flex-start">
-        <Heading as="h1" size="md" textAlign="left">
+        <Heading as="h1" size="ms" textAlign="left">
           등록된 물품이 없어요. <br />
           버튼을 눌러 등록해 보세요.
         </Heading>
-        <Heading as="h3" size="sm" color="GrayText" textAlign="left">
+        <Heading as="h3" size="xs" color="GrayText" textAlign="left">
           누구든지 경매에 참여할 수 있어요.
         </Heading>
 
@@ -20,14 +21,14 @@ export default function AddAuction() {
       <Box flex="1" h={1} />
 
       <Button
-        fontSize="lg"
-        fontWeight="bold"
+        fontSize="md"
+        fontWeight="700"
         bg="#AA8EBF"
         color="white"
         width="240px"
         height="40px"
         onClick={() => {
-          navigate('/auction/create');
+          navigate(`/auction/${id}/create`);
         }}
       >
         등록하기
