20b3a6a42fac26fb1765656753ed924ab9da493c
soyeon <thdusmm6@gmail.com>
2024-10-23 16:26:56 +0900

    feat(경매진행페이지): QueryClientProvider 컴포넌트 위치 변경

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,29 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import router from './routes';
 // import './index.css';
 import theme from './utils/theme';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+      gcTime: 1000 * 60 * 30,
+      retry: false,
+    },
+  },
+});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ChakraProvider theme={theme}>
-      <RouterProvider router={router} />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <RouterProvider router={router} />
+      </ChakraProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
+
+export default queryClient;
--- src/pages/bidPage/bidding.tsx
+++ src/pages/bidPage/bidding.tsx
@@ -1,14 +1,7 @@
 import BidItem from '../../components/bidItem';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-const queryClient = new QueryClient();
 
 function Bidding() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <BidItem />
-    </QueryClientProvider>
-  );
+  return <BidItem />;
 }
 
 export default Bidding;
