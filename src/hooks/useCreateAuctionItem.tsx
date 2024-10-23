1a0856c7051d73285124116f7e7bf2d67fbe2713
giujae <alacran6924@gmail.com>
2024-10-23 14:40:28 +0900

    refactor(AuctionItemCreate(: added navigation after succesful creation

--- src/hooks/useCreateAuctionItem.tsx
+++ src/hooks/useCreateAuctionItem.tsx
@@ -1,4 +1,5 @@
 import { useMutation } from '@tanstack/react-query';
+import { useNavigate } from 'react-router-dom';
 import axiosInstance from '../utils/AxiosInstance';
 import useShowToast from './useShowToast';
 import useAuctionItemStore from '../store/AuntionItemStore';
@@ -8,6 +9,7 @@ const useCreateAuctionItem = () => {
   const { getFormItemData, resetItemForm, updateItemField } =
     useAuctionItemStore();
   const showToast = useShowToast();
+  const nav = useNavigate();
 
   const createAuctionItem = async () => {
     const formData = getFormItemData();
@@ -34,6 +36,7 @@ const useCreateAuctionItem = () => {
       resetItemForm();
       updateItemField('auctionItemId', data);
       showToast('Success', '아이템 생성이 완료되었습니다.', 'success');
+      nav(-1);
     },
     onError: (error) => {
       console.log('에러', error);
