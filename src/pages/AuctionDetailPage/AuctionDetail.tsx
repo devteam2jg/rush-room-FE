8138ba808917a87fcc52e486b24c7d24631899c8
soyeon <thdusmm6@gmail.com>
2024-10-26 23:33:58 +0900

    feat: add privateCodeModal

--- src/pages/AuctionDetailPage/AuctionDetail.tsx
+++ src/pages/AuctionDetailPage/AuctionDetail.tsx
@@ -6,6 +6,7 @@ import AddAuction from '../../components/AddAuction';
 import AuctionList from '../../components/AuctionList';
 import PrivateCodeModal from '../../components/PrivateCodeModal';
 import AuctionHistoryStore from '../../store/AuctionHistoryStore';
+import PrivateCodeModal from '../../components/PrivateCodeModal';
 
 function AuctionDetail() {
   const { auctionId } = useParams();
