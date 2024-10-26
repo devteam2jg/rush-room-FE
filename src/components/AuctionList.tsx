348cc4591b5a7559b2d659499b07532b2952a519
giujae <alacran6924@gmail.com>
2024-10-26 17:08:17 +0900

    feat(AuctionList): fix types

--- src/components/AuctionList.tsx
+++ src/components/AuctionList.tsx
@@ -2,26 +2,7 @@ import { Box, createStandaloneToast, Heading, VStack } from '@chakra-ui/react';
 import { useNavigate } from 'react-router-dom';
 import useAuctionDetail from '../hooks/useAuctionDetail';
 import AuctionItemList from './AuctionItem/AuctionItemList';
-
-interface User {
-  nickname: string;
-  id: string;
-  email: string;
-  profileUrl: string;
-  thumbnailUrl: string;
-}
-
-interface AuctionItem {
-  id: string;
-  title: string;
-  description: string;
-  itemImages: string;
-  startPrice: number;
-  lastPrice: number;
-  isSold: boolean;
-  buyerId: string;
-  postedUser: User;
-}
+import { AuctionItem } from '../utils/types';
 
 export default function AuctionList() {
   const nav = useNavigate();
@@ -43,13 +24,15 @@ export default function AuctionList() {
     });
   }
 
+  console.log(data);
+
   return (
     <Box p={4} textAlign="center" bg="white">
       <VStack spacing={4} align="flex-start">
         <Heading as="h5" size="xm" textAlign="left">
           경매 물품 리스트
         </Heading>
-        {data.items.map((item: AuctionItem) => (
+        {data.items?.map((item: AuctionItem) => (
           <AuctionItemList key={item.id} item={item} />
         ))}
       </VStack>
