8cd1b7280361e374f4a143bf125ec82464af102b
giujae <alacran6924@gmail.com>
2024-10-27 14:57:10 +0900

    feat(Chat): fix listener declaration and disconnection

--- src/components/Bidding/BidChat/ChatInput.tsx
+++ src/components/Bidding/BidChat/ChatInput.tsx
@@ -1,11 +1,18 @@
 import { useParams } from 'react-router-dom';
 import { Socket } from 'socket.io-client';
 import { useEffect, useRef, useState } from 'react';
-import { Button, Input, createStandaloneToast } from '@chakra-ui/react';
+import {
+  Button,
+  Flex,
+  Input,
+  createStandaloneToast,
+  Text,
+  Box,
+} from '@chakra-ui/react';
 import useAuthStore from '../../../store/UserAuthStore';
 
 interface ChatInputProps {
-  socket: Socket;
+  socket: Socket | null;
 }
 
 interface Message {
@@ -22,37 +29,42 @@ function ChatInput({ socket }: ChatInputProps) {
   const [messageList, setMessageList] = useState<Message[]>([]);
   const { auctionId } = useParams();
   const userId = user?.id;
-  const isRef = useRef(0);
+  const endOfMessagesRef = useRef<HTMLDivElement | null>(null);
+
+  const generateColorFrom = (): string => {
+    const lastThree = userId?.slice(-3);
+    return `#${lastThree}${lastThree}`;
+  };
 
   useEffect(() => {
+    if (!socket) return undefined; // socket이 없을 때 early return
+
     socket.on('message', (message: Message) => {
       console.log('received-message', message);
       setMessageList((list) => [...list, message]);
     });
 
     return () => {
-      socket.disconnect();
-      console.log('End Connection');
+      socket.off('message');
+      console.log('socket off');
     };
   }, [socket]);
 
-  const generateColorFrom = (): string => {
-    const lastThree = userId?.slice(-3);
+  const handleSendMessage = () => {
+    // socket이 null이 아닌지 체크
+    if (!socket) {
+      toast({
+        title: '실패',
+        description: '채팅 서버에 연결되지 않았습니다',
+        status: 'error',
+        variant: 'left-accent',
+        duration: 3000,
+        isClosable: true,
+      });
+      return;
+    }
 
-    return `#${lastThree}${lastThree}`;
-  };
-
-  const handleSendMessage = async () => {
-    if (messageSent) {
-      const messageData = {
-        auctionId,
-        userId,
-        nickname: user?.name,
-        message: messageSent,
-      };
-      await socket.emit('message', messageData);
-      console.log(messageList);
-    } else {
+    if (!messageSent) {
       toast({
         title: '실패',
         description: '문자를 입력해 주세요',
@@ -61,7 +73,19 @@ function ChatInput({ socket }: ChatInputProps) {
         duration: 3000,
         isClosable: true,
       });
+      return;
     }
+
+    const messageData = {
+      auctionId,
+      userId,
+      nickname: user?.name,
+      message: messageSent,
+    };
+
+    socket.emit('message', messageData);
+    setMessageSent('');
+    endOfMessagesRef.current?.scrollIntoView({ behavior: 'smooth' });
   };
 
   const handleMessageInput = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -69,19 +93,50 @@ function ChatInput({ socket }: ChatInputProps) {
   };
 
   const userColor = generateColorFrom();
+
   return (
-    <div>
-      {messageList.map((messageContent: Message) => {
-        return (
-          <div key={isRef.current++}>
-            <div color={userColor}>{messageContent.nickname} </div>
-            <div>{messageContent.message}</div>
-          </div>
-        );
-      })}
-      <Input onChange={handleMessageInput} />
-      <Button onClick={handleSendMessage}>전송</Button>
-    </div>
+    <Flex flexDirection="column" height="100%">
+      {/* 채팅 메시지 영역 */}
+      <Flex
+        flex="1"
+        overflowY="auto"
+        flexDirection="column"
+        paddingBottom="62px" // 입력창 높이 + 여유 공간
+      >
+        {messageList.map((messageContent: Message, index) => {
+          return (
+            <Flex
+              textAlign="center"
+              gap="12px"
+              marginBottom="12px"
+              key={`${messageContent.userId}-${index}`}
+            >
+              <Text color={userColor}>{messageContent.nickname}</Text>
+              <Text color="white">{messageContent.message}</Text>
+            </Flex>
+          );
+        })}
+        <Box ref={endOfMessagesRef} />
+      </Flex>
+      <Flex
+        height="50px"
+        backgroundColor="#212326"
+        gap="12px"
+        padding="0 12px"
+        position="absolute"
+        bottom="0"
+        left="0"
+        right="0"
+        width="100%"
+      >
+        <Input
+          color="#ACACAC"
+          value={messageSent}
+          onChange={handleMessageInput}
+        />
+        <Button onClick={handleSendMessage}>전송</Button>
+      </Flex>
+    </Flex>
   );
 }
 
