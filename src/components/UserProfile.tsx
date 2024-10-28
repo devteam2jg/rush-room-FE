20b5e1518d65e298b6250b27867259eba1627574
soyeon <thdusmm6@gmail.com>
2024-10-28 15:58:23 +0900

    feat : add myPage and custom hooks for myPage

---
+++ src/components/UserProfile.tsx
@@ -0,0 +1,55 @@
+import {
+  Heading,
+  Image,
+  Box,
+  Flex,
+  createStandaloneToast,
+} from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
+import useUserProfile from '../hooks/useUserProfile';
+
+function UserProfile() {
+  const nav = useNavigate();
+  const { toast } = createStandaloneToast();
+  const { data, error, isPending } = useUserProfile();
+  if (isPending) {
+    return <div>Loading...!!</div>;
+  }
+
+  if (error) {
+    nav('/');
+    toast({
+      title: '실패',
+      description: `${error.message}`,
+      status: 'error',
+      variant: 'left-accent',
+      duration: 3000,
+      isClosable: true,
+    });
+  }
+  return (
+    <Box w="100%">
+      <Flex
+        p={6}
+        pt={14}
+        backgroundColor="#886CB5"
+        color="white"
+        alignItems="center"
+        justifyContent="space-between"
+        boxShadow="md"
+      >
+        <Heading as="h1" size="lg" fontWeight="900">
+          {data.name}
+        </Heading>
+        <Image
+          src={data.thumbnailUrl}
+          alt="UserProfile"
+          borderRadius="lg"
+          boxSize="100px"
+        />
+      </Flex>
+    </Box>
+  );
+}
+
+export default UserProfile;
--- src/pages/myPage/myPage.tsx
+++ src/pages/myPage/myPage.tsx
@@ -1,3 +1,23 @@
-export default MyPage(){
-    return
+import { Box, Flex } from '@chakra-ui/react';
+import Navigator from '../../components/Navigator';
+import UserProfile from '../../components/UserProfile';
+import UserAuctionStats from '../../components/UserAuctionStats';
+import UserParticipantInfo from '../../components/UserParticipantInfo';
+
+export default function MyPage() {
+  return (
+    <Box minHeight="100vh" backgroundColor="white">
+      <Flex
+        justifyContent="space-between"
+        direction="column"
+        align="center"
+        w="100%"
+      >
+        <UserProfile />
+        <UserAuctionStats />
+        <UserParticipantInfo />
+        <Navigator />
+      </Flex>
+    </Box>
+  );
 }
