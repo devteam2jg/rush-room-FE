b2cecd0c4dc5b8543a8ca54bc49bf83de33cbbfc
giujae <alacran6924@gmail.com>
2024-10-28 22:28:40 +0900

    feat(AuctionItem): add detail page for auction items

--- src/pages/AuctionItemDetailPage/AuctionItemDetail.tsx
+++ src/pages/AuctionItemDetailPage/AuctionItemDetail.tsx
@@ -1,7 +1,38 @@
-import BiddingItemDetail from '../../components/Bidding/BiddingItemDetail';
+import { useNavigate } from 'react-router-dom';
+import { Box, createStandaloneToast, Flex } from '@chakra-ui/react';
+import useBidItemInfo from '../../hooks/useBidItemInfo';
+import BiddingImage from '../../components/Bidding/BiddingImage';
+import AuctionItemInfo from '../../components/AuctionItem/AuctionItemInfo';
 
 function AuctionItemDetail() {
-  return <BiddingItemDetail />;
+  const { data, error, isPending } = useBidItemInfo();
+  const nav = useNavigate();
+  const { toast } = createStandaloneToast();
+
+  if (isPending) {
+    return <div>Loading ...</div>;
+  }
+
+  if (error) {
+    nav('/');
+    toast({
+      title: '실패',
+      description: `${error.message}`,
+      status: 'error',
+      variant: 'left-accent',
+      duration: 3000,
+      isClosable: true,
+    });
+  }
+
+  return (
+    <Flex height="100vh" flexDirection="column">
+      <BiddingImage images={data?.imageUrls} />
+      <Box flex={1}>
+        <AuctionItemInfo />
+      </Box>
+    </Flex>
+  );
 }
 
 export default AuctionItemDetail;
