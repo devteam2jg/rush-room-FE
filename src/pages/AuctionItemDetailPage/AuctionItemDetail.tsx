d93a01d489e9b579ee11cb0e5c7fcc8feef5083f
giujae <alacran6924@gmail.com>
2024-10-28 16:04:11 +0900

    feat(AuctionItemDetail): add page for AuctionItemDetail

---
+++ src/pages/AuctionItemDetailPage/AuctionItemDetail.tsx
@@ -0,0 +1,7 @@
+import BiddingItemDetail from '../../components/Bidding/BiddingItemDetail';
+
+function AuctionItemDetail() {
+  return <BiddingItemDetail />;
+}
+
+export default AuctionItemDetail;
