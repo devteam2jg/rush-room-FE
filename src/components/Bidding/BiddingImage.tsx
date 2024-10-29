2391d6f2369530ab544ab3169ed45d0655ae5e69
giujae <alacran6924@gmail.com>
2024-10-29 16:34:24 +0900

    feat(Bidding): added modal on click picture of auction item

--- src/components/Bidding/BiddingImage.tsx
+++ src/components/Bidding/BiddingImage.tsx
@@ -1,27 +1,91 @@
-import { Box, Image } from '@chakra-ui/react';
-import { Pagination } from 'swiper/modules';
+import {
+  Box,
+  Image,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalCloseButton,
+  useDisclosure,
+} from '@chakra-ui/react';
+import { Pagination, Navigation } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { nanoid } from 'nanoid';
+import { useState } from 'react';
+import 'swiper/css/navigation';
 
 interface BiddingImageProps {
   images: string[];
 }
 
 function BiddingImage({ images }: BiddingImageProps) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [initialSlide, setInitialSlide] = useState(0);
+
+  const handleImageClick = (index: number) => {
+    setInitialSlide(index);
+    onOpen();
+  };
+
   return (
     <Box backgroundColor="#141517">
       <Swiper pagination modules={[Pagination]} className="mySwiper">
-        {images.map((image) => (
+        {images.map((image, index) => (
           <SwiperSlide key={nanoid()}>
             <Image
               height="35vh"
               objectFit="cover"
               margin="0 auto"
               src={image}
+              cursor="pointer"
+              onClick={() => handleImageClick(index)}
             />
           </SwiperSlide>
         ))}
       </Swiper>
+
+      <Modal isOpen={isOpen} onClose={onClose} size="full" isCentered>
+        <ModalOverlay />
+        <ModalContent
+          bg="rgba(0, 0, 0, 0.9)"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          maxW="100vw"
+          h="calc(var(--vh, 1vh) * 100)"
+        >
+          <ModalCloseButton color="white" zIndex="modal" />
+          <Swiper
+            initialSlide={initialSlide}
+            pagination
+            navigation
+            modules={[Pagination, Navigation]}
+            className="mySwiper"
+            style={{
+              width: '100%',
+              height: '100%',
+            }}
+          >
+            {images.map((image) => (
+              <SwiperSlide
+                key={nanoid()}
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+              >
+                <Image
+                  src={image}
+                  maxH="90vh"
+                  maxW="90vw"
+                  objectFit="contain"
+                  onClick={(e) => e.stopPropagation()}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 }
