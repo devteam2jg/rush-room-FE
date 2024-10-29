f77567851ff5dc93dcd845a308b911b819a82c72
giujae <alacran6924@gmail.com>
2024-10-29 16:32:50 +0900

    refactor: fixed issue of mobile browser's bottom bar being included in range 100vh

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import './App.css';
-
-import { Button, useColorMode } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
+import { RouterProvider } from 'react-router-dom';
+import { useEffect } from 'react';
+import theme from './utils/theme';
+import router from './routes';
 
 function App() {
-  const { toggleColorMode } = useColorMode();
-  return <Button onClick={toggleColorMode} />;
+  function setScreenSize() {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
+  useEffect(() => {
+    setScreenSize();
+  });
+  return (
+    <ChakraProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ChakraProvider>
+  );
 }
 
 export default App;
