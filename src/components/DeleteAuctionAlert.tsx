1c98905027d26a631106a9fdad384ff91dcdb350
soyeon <thdusmm6@gmail.com>
2024-10-30 12:52:47 +0900

    feat: add delete alert in auction info Page

---
+++ src/components/DeleteAuctionAlert.tsx
@@ -0,0 +1,47 @@
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogCloseButton,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  Button,
+  useDisclosure,
+} from '@chakra-ui/react';
+import React from 'react';
+
+export default function DeleteAuction() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const cancelRef = React.useRef();
+
+  return (
+    <>
+      <Button colorScheme="red" onClick={onOpen}>
+        삭제하기
+      </Button>
+
+      <AlertDialog
+        motionPreset="slideInBottom"
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+        isOpen={isOpen}
+        isCentered
+      >
+        <AlertDialogOverlay />
+
+        <AlertDialogContent>
+          <AlertDialogHeader>경매 삭제하기</AlertDialogHeader>
+          <AlertDialogCloseButton />
+          <AlertDialogBody>경매를 삭제하시겠습니까?</AlertDialogBody>
+          <AlertDialogFooter>
+            <Button onClick={onClose}>No</Button>
+            <Button colorScheme="red" ml={3}>
+              Yes
+            </Button>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
+  );
+}
