1474f0ad07b0b02b356382d28352ce26dd1b8c20
soyeon <thdusmm6@gmail.com>
2024-10-30 15:23:33 +0900

    feat: implement delete auction

--- src/components/DeleteAuctionAlert.tsx
+++ src/components/DeleteAuctionAlert.tsx
@@ -10,10 +10,20 @@ import {
   useDisclosure,
 } from '@chakra-ui/react';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import useDeleteAuction from '../hooks/useDeleteAuction';
 
 export default function DeleteAuction() {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const cancelRef = React.useRef();
+  const cancelRef = React.useRef<HTMLButtonElement>(null);
+  const nav = useNavigate();
+  const { deleteAuction } = useDeleteAuction();
+
+  const handleDeleteAuction = () => {
+    deleteAuction();
+    nav(`/overview`);
+    console.log('경매 삭제 완료');
+  };
 
   return (
     <>
@@ -36,7 +46,7 @@ export default function DeleteAuction() {
           <AlertDialogBody>경매를 삭제하시겠습니까?</AlertDialogBody>
           <AlertDialogFooter>
             <Button onClick={onClose}>No</Button>
-            <Button colorScheme="red" ml={3}>
+            <Button colorScheme="red" ml={3} onClick={handleDeleteAuction}>
               Yes
             </Button>
           </AlertDialogFooter>
