9ddeb0c06b23a169fc5b2a22a94c20c8c168139e
giujae <alacran6924@gmail.com>
2024-11-04 23:18:47 +0900

    feat(stream): add temporal image will be replaced later

---
+++ src/components/Bid/BiddingStream.tsx
@@ -0,0 +1,14 @@
+import { Image } from '@chakra-ui/react';
+
+function BiddingStream() {
+  return (
+    <Image
+      display="block"
+      width="100%"
+      height="100%"
+      src="/images/back_temp.jpeg"
+    />
+  );
+}
+
+export default BiddingStream;
