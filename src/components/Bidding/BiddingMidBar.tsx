69ef72d9ff0e9bb559b5e6b0e9beecfa018cc883
giujae <alacran6924@gmail.com>
2024-11-04 23:20:25 +0900

    feat(Bid): add component showing time left

--- src/components/Bidding/BiddingMidBar.tsx
+++ src/components/Bidding/BiddingMidBar.tsx
@@ -1,10 +1,31 @@
 import { Flex, Text } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
 
 interface BiddingMidBarProps {
   currentPrice: number;
 }
 
 function BiddingMidBar({ currentPrice }: BiddingMidBarProps) {
+  const [timeLeft, setTimeLeft] = useState(600); // 10분 = 600초
+
+  useEffect(() => {
+    if (timeLeft <= 0) return undefined; // 시간이 다 되면 중지
+    const timer = setInterval(
+      () => setTimeLeft((prevTime) => prevTime - 1),
+      1000
+    );
+
+    return () => clearInterval(timer); // 컴포넌트 언마운트 시 타이머 정리
+  }, [timeLeft]);
+
+  const formatTime = (seconds: number): string => {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const remainingSeconds = seconds % 60;
+
+    return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
+  };
+
   return (
     <Flex
       backgroundColor="#141517"
@@ -18,7 +39,7 @@ function BiddingMidBar({ currentPrice }: BiddingMidBarProps) {
         fontSize="17px"
         fontWeight={700}
       >
-        12:54:30
+        {formatTime(timeLeft)}
       </Text>
       <Flex gap="8px" justifyContent="center" alignContent="center">
         <Text color="white" fontSize="17px">
