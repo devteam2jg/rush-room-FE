e0f2dc6751b6e5b4288ec007452b71aec06dfbf1
soyeon <thdusmm6@gmail.com>
2024-11-05 20:57:08 +0900

    feat(AutionDetail) : add functionality for reordering auction items

--- src/components/AuctionList.tsx
+++ src/components/AuctionList.tsx
@@ -1,5 +1,6 @@
 import { Box, createStandaloneToast, Heading, VStack } from '@chakra-ui/react';
 import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import useAuctionDetail from '../hooks/useAuctionDetail';
 import AuctionItemList from './AuctionItem/AuctionItemList';
 import { AuctionItem } from '../utils/types';
@@ -18,6 +19,16 @@ export default function AuctionList({
   const nav = useNavigate();
   const { toast } = createStandaloneToast();
   const { data, error, isPending } = useAuctionDetail();
+  const [items, setItems] = useState<AuctionItem[]>([]);
+  const [draggingItem, setDraggingItem] = useState<AuctionItem | null>(null);
+  const [dragOverIndex, setDragOverIndex] = useState<number | null>(null);
+
+  useEffect(() => {
+    if (data) {
+      setItems(data.items);
+    }
+  }, [data]);
+
   if (isPending) {
     return <div>Loading...!!</div>;
   }
@@ -33,6 +44,41 @@ export default function AuctionList({
       isClosable: true,
     });
   }
+
+  // 드래그가 실행될 때 호출
+  const handleDragStart = (item: AuctionItem) => {
+    console.log('Drag start', item);
+    setDraggingItem(item);
+  };
+
+  // 드래그 중인 요소가 다른 요소 위에 올라갈 때
+  const handleDragOver = (
+    e: React.DragEvent<HTMLDivElement>,
+    index: number
+  ) => {
+    console.log('Drag Over', index);
+    e.preventDefault();
+    setDragOverIndex(index);
+  };
+
+  // 드롭 시 호출
+  const handleDrop = () => {
+    console.log('Drop:', draggingItem);
+    if (draggingItem !== null && dragOverIndex !== null) {
+      const updatedItems = [...items];
+      const draggedIndex = updatedItems.findIndex(
+        (i) => i.id === draggingItem.id
+      );
+
+      if (draggedIndex !== -1 && draggedIndex !== dragOverIndex) {
+        const [removedItem] = updatedItems.splice(draggedIndex, 1);
+        updatedItems.splice(dragOverIndex, 0, removedItem);
+        setItems(updatedItems);
+      }
+    }
+    setDraggingItem(null);
+    setDragOverIndex(null);
+  };
 
   return (
     <Box
@@ -52,8 +98,21 @@ export default function AuctionList({
         >
           경매 물품 리스트
         </Heading>
-        {data.items?.map((item: AuctionItem) => (
-          <AuctionItemList key={item.id} item={item} />
+        {items.map((item, index) => (
+          <div
+            key={item.id}
+            draggable
+            onDragStart={() => handleDragStart(item)}
+            onDragOver={(e) => handleDragOver(e, index)}
+            onDrop={handleDrop}
+            style={{
+              border: dragOverIndex === index ? '2px dashed #AA8EBF' : 'none',
+              padding: '4px',
+              zIndex: 0,
+            }}
+          >
+            <AuctionItemList item={item} />
+          </div>
         ))}
       </VStack>
     </Box>
