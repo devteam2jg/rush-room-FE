79e9413bec788886f81e22d1dae9ac3faa921a8c
giujae <alacran6924@gmail.com>
2024-11-06 19:21:39 +0900

    Chores(Bid): minor updates

--- src/hooks/Bid/useSocketOnEnter.tsx
+++ src/hooks/Bid/useSocketOnEnter.tsx
@@ -1,30 +1,51 @@
 import { createStandaloneToast } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 import { io, Socket } from 'socket.io-client';
+import useAuthStore from '../../store/UserAuthStore';
 
 interface SocketProps {
   auctionId: string | undefined;
-  itemId: string | undefined;
 }
 
-const useSocketOnEnter = ({ auctionId, itemId }: SocketProps) => {
+interface ResponseType {
+  itemId: string;
+  time: number;
+  bidPrice: number;
+  bidderId: string;
+}
+
+const useSocketOnEnter = ({ auctionId }: SocketProps) => {
   const [socket, setSocket] = useState<Socket | null>(null);
   const baseURL = import.meta.env.VITE_APP_SOCKET_URL;
   const { toast } = createStandaloneToast();
   const [isConnected, setIsConnected] = useState(false);
+  const [initialInfo, setInitialInfo] = useState<ResponseType>();
+  const user = useAuthStore((state) => state.user);
 
   useEffect(() => {
-    if (!auctionId || !itemId) return undefined;
+    if (!auctionId) return undefined;
 
     const newSocket = io(`${baseURL}/auction-execute`);
     setSocket(newSocket);
 
     const sendAuctionId = {
       auctionId,
-      itemId,
+      userId: user?.id,
     };
 
+    console.log('들어온사람', user?.id);
+    console.log('들어온사람', user?.name);
+
     newSocket.emit('join_auction', sendAuctionId);
+
+    const sendonEnter = {
+      auctionId,
+      type: null,
+    };
+
+    newSocket.emit('INFO', sendonEnter, (response: ResponseType) => {
+      setInitialInfo(response);
+    });
 
     newSocket.on('connect', () => {
       toast({
@@ -41,13 +62,14 @@ const useSocketOnEnter = ({ auctionId, itemId }: SocketProps) => {
 
     return () => {
       socket?.disconnect();
+      socket?.off('connect');
       setSocket(null);
       setIsConnected(false);
       console.log('End Socket Connection!');
     };
   }, [auctionId]);
 
-  return { socket, isConnected };
+  return { socket, isConnected, initialInfo };
 };
 
 export default useSocketOnEnter;
