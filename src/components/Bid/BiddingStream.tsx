26cc3562047b45d8d5e40c67a2607d1bb7eb0e6e
giujae <alacran6924@gmail.com>
2024-11-09 19:47:53 +0900

    feat(Stream): add stream flow control logic

--- src/components/Bid/BiddingStream.tsx
+++ src/components/Bid/BiddingStream.tsx
@@ -1,20 +1,133 @@
-import { Image } from '@chakra-ui/react';
+import { Box, Button, Flex, HStack, Text } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import useAuthStore from '../../store/UserAuthStore';
 import RTCTestPage from '../../pages/RTCTestPage';
+import SpringModal from '../Modal/SpringModal';
+import VideoSocketStore from '../../store/VideoSocketStore';
+import useSocketStore from '../../store/useSocketStore';
 
-interface StreamProps {
-  isOwner: boolean;
-}
+function BiddingStream() {
+  const socket = useSocketStore((state) => state.socket);
+  const isConnected = useSocketStore((state) => state.isConnected);
+  const videoSocket = VideoSocketStore((state) => state.socket);
+  const isVideoConnected = VideoSocketStore((state) => state.isConnected);
+  const { auctionId } = useParams();
+  const [isOpen, setIsOpen] = useState(false);
+  const [isOwner, setIsOwner] = useState(false);
+  const [cameraOff, setCameraOff] = useState(false);
+  const [sellerId, setSellerId] = useState('');
+  const [confirmed, setConfirmed] = useState(false);
+  const user = useAuthStore((state) => state.user);
 
-function BiddingStream({ isOwner }: StreamProps) {
+  useEffect(() => {
+    const checkOwnership = () => {
+      console.log('sellerId', sellerId);
+      console.log('myid', user?.id);
+      const ownerStatus = sellerId === user?.id;
+      setIsOwner(ownerStatus);
+
+      if (ownerStatus) {
+        setIsOpen(true);
+        if (confirmed) {
+          setIsOpen(false);
+        }
+      }
+    };
+
+    checkOwnership();
+    setCameraOff(false);
+  }, [sellerId]);
+
+  useEffect(() => {
+    if (!socket) return undefined;
+
+    const handleSellerId = (response: any) => {
+      const { type } = response;
+
+      if (type === 'BID_READY') {
+        setConfirmed(false);
+        setIsOwner(false);
+        setSellerId(response.sellerId);
+      } else if (type === 'BID_END') {
+        setCameraOff(true);
+        setConfirmed(false);
+      }
+    };
+
+    socket.on('NOTIFICATION', handleSellerId);
+
+    return () => {
+      socket.off('NOTIFICATION', handleSellerId);
+    };
+  }, [socket]);
+
+  if (!isConnected || !isVideoConnected) {
+    return <div>Loading...</div>;
+  }
+
+  console.log('지나왔어');
+
+  const handleConfirm = () => {
+    if (!videoSocket) return;
+    setConfirmed(true);
+    const agreementSend = {
+      roomId: auctionId,
+      isAgreed: true,
+    };
+    console.log('동의할게?', confirmed);
+    videoSocket.emit('seller-agreed', agreementSend);
+    setIsOpen(false);
+  };
+
+  const handleReject = () => {
+    if (!videoSocket) return;
+    setConfirmed(false);
+    const agreementSend = {
+      roomId: auctionId,
+      isAgreed: false,
+    };
+    console.log('거절할게?');
+    videoSocket.emit('seller-agreed', agreementSend);
+    setIsOpen(false);
+  };
+
   return (
-    // <Image
-    //   display="block"
-    //   width="100%"
-    //   height="100%"
-    //   src="/images/back_temp.jpeg"
-    //   objectFit="cover"
-    // />
-    <RTCTestPage isOwner={isOwner} />
+    <Box width="100%" height="100%">
+      <SpringModal p="p-10" isOpen={isOpen} setIsOpen={setIsOpen}>
+        <Box padding="12px" color="#FCFCFD" bg="#282828" width="100%">
+          알림
+        </Box>
+        <Flex
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+          padding="15px"
+          color="#FCFCFD"
+          bg="#222222"
+          gap={5}
+        >
+          <Text fontSize={{ base: '20px' }}>물건을 팔아 볼 차례예요.</Text>
+          <Text fontSize={{ base: '18px' }}>
+            판매를 위해 카메라를 켜주세요!
+          </Text>
+          <HStack gap={5} justifyContent="center" width="100%">
+            <Button onClick={handleConfirm} width="100px" colorScheme="mong">
+              동의
+            </Button>
+            <Button
+              onClick={handleReject}
+              width="100px"
+              colorScheme="mongCancle"
+            >
+              거부
+            </Button>
+          </HStack>
+        </Flex>
+      </SpringModal>
+
+      <RTCTestPage cameraOff={cameraOff} isOwner={isOwner} />
+    </Box>
   );
 }
 
