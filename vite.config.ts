17750a966ca307cbaa22151ad577afb53f27673d
smart-cau <1995hyunwoo@naver.com>
2024-11-10 14:30:45 +0900

    temp 주석

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+/* --DEV SERVER -- */
 export default defineConfig({
   plugins: [react()],
-  base: 'https://dev.rushroom.kr/',
+  base: 'https://dev.rushroom.kr/', // prod server에는 'dev.' 떼면 끝. 그리고 .env.production 파일에서도 .dev 다 떼면 됨
   build: {
     chunkSizeWarningLimit: 1600,
     manifest: true,
@@ -15,3 +16,22 @@ export default defineConfig({
     },
   },
 });
+
+/* -- LOCAL DEV SERVER -- */
+// import { defineConfig, loadEnv } from 'vite';
+// import react from '@vitejs/plugin-react';
+//
+// // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => {
+//   // Load environment variables based on the mode (e.g., 'development', 'production')
+//   const env = loadEnv(mode, process.cwd(), '');
+//
+//   return {
+//     base: '/', // 이것은 배포 시의 public path
+//     plugins: [react()],
+//     server: {
+//       host: true, // 개발 서버 호스트
+//       port: env.VITE_PORT || 3000,
+//     },
+//   };
+// });
