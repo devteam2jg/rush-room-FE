e56e4beb0482d391cb90eaaeff4895925667c3c2
soyeon <thdusmm6@gmail.com>
2024-11-11 05:10:37 +0900

    feat: add onclick method on logo image

--- src/pages/AuctionOverviewPage/AuctionOverview.tsx
+++ src/pages/AuctionOverviewPage/AuctionOverview.tsx
@@ -1,13 +1,16 @@
 import { Image, Flex, Box } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
 import Navigator from '../../components/Navigator';
 import serviceLogo from '../../assets/images/serviceLogo.png';
 import AuctionOverviewList from '../../components/AuctionOverviewList';
 
 export default function AuctionOverview() {
+  const nav = useNavigate();
+
   return (
     <Box height="calc(var(--vh, 1vh) * 100)" position="relative">
       <Flex backgroundColor="#161717" justifyContent="space-between">
-        <Image height="50px" src={serviceLogo} />
+        <Image height="50px" src={serviceLogo} onClick={() => nav('/')} />
       </Flex>
       <AuctionOverviewList />
       <Navigator />
